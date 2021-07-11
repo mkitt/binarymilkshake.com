9cc6193ea9c6e5e74440d2c99f5002a767c4cf23
Matthew Kitt <mk@mkitt.net>
2021-07-11 06:21:53 -0600

    Package updates and enable JIT for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
+  mode: 'jit',
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media',
   theme: {
