930affa432938ace0504695ec2ec35081debbc3b
Matthew Kitt <mk@mkitt.net>
2023-08-14 13:40:24 -0600

    Add copy

--- app/colorado/the-aframe/page.tsx
+++ app/colorado/the-aframe/page.tsx
@@ -26,32 +26,64 @@ export default async function Page() {
         <p>Winter Park, Colorado</p>
       </header>
       <Carousel slides={slides} />
-      <section className="flex max-w-4xl">
+      <section className="mt-8 flex max-w-4xl">
         <div className="flex flex-col gap-4 p-4 text-sm">
-          <h2 className="text-xl font-600">A heading of things</h2>
+          <div>
+            <h2 className="text-xl font-600">
+              6 month Seasonal Rental - Sleeps 2-5
+            </h2>
+            <h3 className="text-lg font-600">
+              Nov 1 thru April 2024 | $2,000/mo | Furnished
+            </h3>
+            <p>
+              <strong>232 Marble Road, Tabernash CO 80478</strong>
+            </p>
+          </div>
+
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            dapibus venenatis neque, ut fermentum mi bibendum vel. Sed non
-            risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-            nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
-            ligula massa, varius a, semper congue, euismod non, mi.
+            Scandinavian design Aframe with wood stove, cozy living area and
+            large back deck. Property backs up to open space with the YMCA of
+            the Rockies accessible right out the back door to the west, 5 min
+            walk to trails for hiking, snowshoeing, mtn biking, x-country
+            skiing. Also backs up to 6th hole fare way of The Ridge course on
+            Pole Creek Golf Course.
           </p>
-          <p>
-            Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nullam
-            dignissim risus nec enim. Vestibulum volutpat pretium libero. Cras
-            id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla
-            eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede.
-          </p>
-          <p>
-            Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.
-            Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper
-            libero, sit amet adipiscing sem neque sed ipsum.
-          </p>
-          <p>
-            Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
-            quis gravida magna mi a libero. Vivamus sagittis lacus vel augue
-            laoreet rutrum.
-          </p>
+
+          <ul>
+            <li>1080 sq ft</li>
+            <li>1 bedroom with queen bed</li>
+            <li>
+              office on upper level has full size pull out couch with featherbed
+            </li>
+            <li>couch downstairs also could sleep 1 (twin bed sized)</li>
+            <li>laundry in entry closet</li>
+            <li>ski storage in entry</li>
+            <li>2 bathrooms</li>
+            <li>- 1 is a powder room with commode/sink</li>
+            <li>- 1 has full size shower + sink</li>
+            <li>large backyard deck with grill</li>
+            <li>outdoor fire pit</li>
+          </ul>
+
+          <ul>
+            <li>includes century link internet</li>
+            <li>plowing</li>
+            <li>wood for wood stove</li>
+            <li>trash (we share with neighbor)</li>
+          </ul>
+
+          <div>
+            <p>not included:</p>
+            <ul>
+              <li>
+                utilities (electric - we will leave in our name and bill you)
+              </li>
+              <li>
+                tv service (we use streaming service through apple tv, tv in
+                photos will stay)
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </main>
